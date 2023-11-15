046c5fb5051ca5e3c66979b94011d1ea987b2969
tato jakhua <anatoli.jakhua@gmail.com>
2023-11-15 12:26:23 -0800

    new project && skils

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Tato</span>
+            Hi, I'm <span className="text-[#915EFF]">Anatoli</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop React application <br className="sm:block hidden" />
