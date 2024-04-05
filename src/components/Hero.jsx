e3a6a4faf09b77e07b21c3b491399677f0040787
tatojakhua <anatoli.jakhua@gmail.com>
2024-04-05 14:53:09 +0400

    change word aplications

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Tato</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            JavaScript Developers <br className="sm:block hidden" />
+            I develop React applications <br className="sm:block hidden" />
             using JavaScript
           </p>
         </div>
